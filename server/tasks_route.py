87e9c8261defa87b08d141e41a7855f4fd501359
Mohammad-Aljafary <mhm0222260@ju.edu.jo>
2026-07-17 18:29:11 +0300

    Refactor Task model and enhance task creation logic

--- server/tasks_route.py
+++ server/tasks_route.py
@@ -9,10 +9,12 @@ router = APIRouter(
 )
 
 class Task(BaseModel):
-    id: int
+    id: int 
+    title: str 
+    done: bool 
+
+class TaskCreate(BaseModel):
     title: str
-    description: str
-    done: bool
 
 tasks = []
 
@@ -27,3 +29,15 @@ async def read_task(task_id: int) -> Task:
         if task.id == task_id:
             return task
     raise HTTPException(status_code=404, detail=f"Task {task_id} not found")
+
+@router.post("/", status_code=201)
+async def create_task(task: TaskCreate) -> Task:
+    if task.title == None or task.title.strip() == "":
+        raise HTTPException(status_code=400, detail="Task title cannot be empty")
+    new_task = Task(
+        id=len(tasks) + 1,
+        title=task.title,
+        done=False
+    )
+    tasks.append(new_task)
+    return new_task
